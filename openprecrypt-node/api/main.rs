d4d574d06ad9f53b14aad505278c344224532297
Jacob Peddicord <jacob.peddicord@hey.com>
2022-02-02 15:13:21 -0500

    test new vercel setup

---
+++ openprecrypt-node/api/main.rs
@@ -0,0 +1,34 @@
+use http::StatusCode;
+use now_lambda::{error::NowError, lambda, IntoResponse, Request, Response};
+
+fn get_response(req: Request) -> Result<serde_json::Value, &'static str> {
+    return json!({
+        data: "hello"
+    });
+    // let body: Value = match req.body() {
+    //     now_lambda::Body::Binary(data) => {
+    //         serde_json::from_slice(data).map_err(|_| "request body is not valid json")?
+    //     }
+    //     _ => return Err("Request body is not in binary format"),
+    // };
+}
+
+fn handler(req: Request) -> Result<impl IntoResponse, NowError> {
+    match get_response(req) {
+        Ok(res) => Ok(Response::builder()
+            .status(StatusCode::OK)
+            .header("Content-Type", "application/json")
+            .body(res.to_string())
+            .expect("Something happened")),
+
+        Err(e) => Ok(Response::builder()
+            .status(StatusCode::OK)
+            .body(e.into())
+            .expect("Something happened")),
+    }
+}
+
+// Start the runtime with the handler
+fn main() -> Result<(), Box<dyn Error>> {
+    Ok(lambda!(handler))
+}
