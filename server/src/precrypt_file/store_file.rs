c7a3bc0405cdcf6a06f85dbaecc262756b9b085e
Jacob Peddicord <jacob.peddicord@hey.com>
2022-02-09 09:33:47 -0500

    make store file work for tiny files

--- server/src/precrypt_file/store_file.rs
+++ server/src/precrypt_file/store_file.rs
@@ -1,7 +1,9 @@
+use actix_web::client::Client;
 use precrypt::{precrypt, RecryptionKeys};
+use serde_json::{json, Value};
 use std::ffi::OsStr;
 use std::fs;
-use std::io::Write;
+use std::path::Path;
 use umbral_pre::*;
 
 use crate::precrypt_key::*;
@@ -35,22 +37,47 @@ pub async fn store(
    .unwrap();
 
    // Upload encrypted file to IPFS
-   // TODO
+   // TODO: Make this work for files > 100MB
+   println!("Storing cipher...");
+   let cipher_bytes = fs::read(cipher_file_path).unwrap();
+   let cipher_str = serde_json::to_string(&cipher_bytes).unwrap();
+   let client = Client::default();
+   let file_response = client
+      .post("https://api.web3.storage/upload")
+      .header("authorization", format!("Bearer {}", web3_token))
+      .timeout(std::time::Duration::new(20, 0))
+      .send_body(&cipher_str)
+      .await;
+   println!("{:?}", file_response);
+   let file_response_str = file_response.unwrap().body().await.unwrap();
+   let file_response_json: Value = serde_json::from_slice(&file_response_str).unwrap();
 
    // Store Key
+   println!("Storing key...");
    let recryption_keys_array = std::fs::read(recrypt_key_string).unwrap();
    let recryption_keys: RecryptionKeys = serde_json::from_slice(&recryption_keys_array).unwrap();
    let key_store = store_key::KeyStoreRequest {
       recryption_keys: recryption_keys,
       mint: mint,
    };
-   let response = store_key::store(key_store, orion_secret, web3_token)
+   let key_response_json = store_key::store(key_store, orion_secret, web3_token)
       .await
       .unwrap();
-   
    // Cleanup created files
    fs::remove_dir_all(&file_uuid).unwrap();
 
    // Write file CID and key CID to json in the folder with an expiration time
-   std::fs::write("result.json", serde_json::to_string(&response).unwrap()).unwrap();
+   if !Path::new("results").is_dir() {
+      fs::create_dir("results").unwrap();
+   }
+   let result_file_str = &format!("results/{},json", file_uuid);
+   std::fs::write(
+      result_file_str,
+      serde_json::to_string(&json!({
+         "file_response": file_response_json,
+         "key_response": key_response_json
+      }))
+      .unwrap(),
+   )
+   .unwrap();
 }
--- server/src/precrypt_key/store_key.rs
+++ server/src/precrypt_key/store_key.rs
@@ -1,7 +1,7 @@
-use precrypt::RecryptionKeys;
-use orion::aead;
-use serde::{Deserialize, Serialize};
 use actix_web::client::Client;
+use orion::aead;
+use precrypt::RecryptionKeys;
+use serde::{Deserialize, Serialize};
 use serde_json::value::Value;
 
 #[derive(Serialize, Deserialize)]
@@ -10,22 +10,31 @@ pub struct KeyStoreRequest {
     pub mint: String,
 }
 
-pub async fn store(key_store_request: KeyStoreRequest, orion_secret: String, web3_token: String) -> std::io::Result<Value> {
-   // Encrypt the data before storing on IPFS
-   let secret_slice: Vec<u8> = serde_json::from_str(&orion_secret).unwrap();
-   let secret_key = aead::SecretKey::from_slice(&secret_slice).unwrap();
-   let cipher_bytes = aead::seal(&secret_key, &serde_json::to_vec(&key_store_request).unwrap()).unwrap();
-   let cipher_str = serde_json::to_string(&cipher_bytes).unwrap();
+pub async fn store(
+    key_store_request: KeyStoreRequest,
+    orion_secret: String,
+    web3_token: String,
+) -> std::io::Result<Value> {
+    // Encrypt the data before storing on IPFS
+    let secret_slice: Vec<u8> = serde_json::from_str(&orion_secret).unwrap();
+    let secret_key = aead::SecretKey::from_slice(&secret_slice).unwrap();
+    let cipher_bytes = aead::seal(
+        &secret_key,
+        &serde_json::to_vec(&key_store_request).unwrap(),
+    )
+    .unwrap();
+    let cipher_str = serde_json::to_string(&cipher_bytes).unwrap();
 
-   // Store the data on IPFS with web3.storage
-   let client = Client::default();
-   let response = client
-       .post("https://api.web3.storage/upload")
-       .header("authorization", format!("Bearer {}", web3_token))
-       .send_json(&cipher_str)
-       .await;
-
-   let response_body_str = response.unwrap().body().await.unwrap();
-   let response_body: Value = serde_json::from_slice(&response_body_str).unwrap();
-   return Ok(response_body);
+    // Store the data on IPFS with web3.storage
+    let client = Client::default();
+    let response = client
+        .post("https://api.web3.storage/upload")
+        .header("authorization", format!("Bearer {}", web3_token))
+        .timeout(std::time::Duration::new(20, 0))
+        .send_json(&cipher_str)
+        .await;
+    println!("{:?}", response);
+    let response_body_str = response.unwrap().body().await.unwrap();
+    let response_body: Value = serde_json::from_slice(&response_body_str).unwrap();
+    return Ok(response_body);
 }
