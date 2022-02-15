d046039dc6398f393d86ffc2442c64b1d9a22f75
Jacob Peddicord <jacob.peddicord@hey.com>
2022-02-15 14:30:24 -0500

    better command runner

---
+++ server/src/util/command.rs
@@ -0,0 +1,16 @@
+use std::process::Command;
+
+#[derive(Debug, Clone)]
+struct CommandError;
+
+pub fn run_command(cmd: &str) -> std::io::Result<std::process::Output> {
+   let result = Command::new("sh")
+      .arg("-c")
+      .arg(cmd)
+      .output()?;
+   if !result.stderr.is_empty() {
+      let error = std::str::from_utf8(&result.stderr).unwrap();
+      return Err(std::io::Error::new(std::io::ErrorKind::Other, error));
+   }
+   return Ok(result);
+}
