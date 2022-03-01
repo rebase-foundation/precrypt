ae2799dfc14c4bdd98f298bab4cbfa8466b95fd9
Jacob Peddicord <jacob.peddicord@hey.com>
2022-03-01 14:27:42 -0500

    binary building

---
+++ precrypt/npm/getBinary.js
@@ -0,0 +1,34 @@
+const { Binary } = require('binary-install');
+const os = require('os');
+
+function getPlatform() {
+	const type = os.type();
+	const arch = os.arch();
+
+	if (type === 'Windows_NT') {
+		if (arch === 'x64') {
+			return 'win64';
+		} else {
+			return 'win32';
+		}
+	}
+
+	if (type === 'Linux' && arch === 'x64') {
+		return 'linux';
+	}
+
+	if (type === 'Darwin' && arch === 'x64') {
+		return 'macos';
+	}
+
+	throw new Error(`Unsupported platform: ${type} ${arch}. Please create an issue at https://github.com/woubuc/sweep/issues`);
+}
+
+function getBinary() {
+	const platform = getPlatform();
+	const version = require('../package.json').version;
+	const url = `https://github.com/rebase-foundation/precrypt/releases/download/v${ version }/precrypt-${ platform }.tar.gz`;
+	return new Binary(url, { name: 'precrypt' });
+}
+
+module.exports = getBinary;
